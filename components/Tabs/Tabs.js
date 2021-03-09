ee7b57c5f1d980c068495302202c50f2563baab1
xiaoye <1509983667@qq.com>
2021-03-09 21:17:03 +0800

    Preparation before Pagination

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -21,6 +21,13 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    // 点击事件
+    handleItemTap(e){
+      // 1. 获取点击的索引
+      const { index } = e.currentTarget.dataset;
+      // console.log(e.currentTarget.dataset);
+      // 2. 处罚 父组件中的事件 自定义
+    this.triggerEvent("tabsItemChange",{index})
+    }
   }
 })
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,4 +1,6 @@
-// pages/goods_list/index.js
+import{ request } from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
+
 Page({
 
   /**
@@ -9,74 +11,56 @@ Page({
       {
         id: 0,
         value: "综合",
-        isactive: true
+        isActive: true
       },
       {
         id: 1,
         value: "销量",
-        isactive: false
+        isActive: false
       },
       {
         id: 2,
         value: "价格",
-        isactive: false
+        isActive: false
       }
-    ]
+    ],
+    goodsList:[]
+  },
+
+  // 接口要的参数
+  QueryParams: {
+    query: "",
+    cid: "",
+    pagenum: 1,
+    pagesize: 10
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options);
+    this.QueryParams.cid = options.cid;
+    this.getGoodsList();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 获取商品列表数据
+  async getGoodsList(){
+    const res = await request({ url:"/goods/search", data: this.QueryParams})
+    this.setData({
+      goodsList: res.goods
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 标题的点击事件 从子组件传递过来
+  handleTabsItemChange(e){
+    // 1. 获取被点击的标题索引
+    const { index } = e.detail;
+    // 2. 修改原数组
+    let { tabs } = this.data;
+    tabs.forEach((v,i) => i===index?v.isActive=true:v.isActive=false);
+    // 3. 赋值到data中
+    this.setData({
+      tabs
+    })
   }
 })
